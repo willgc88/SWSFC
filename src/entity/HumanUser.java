a7486029bca55551233b0bfb2f9dff745876dadc
sultan <nnurapins@gmail.com>
2023-10-23 17:58:41 -0400

    added user

---
+++ src/entity/HumanUser.java
@@ -0,0 +1,24 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+public class HumanUser implements User{
+
+    private final String username;
+    private final String password;
+
+    /**
+     * Requires: password is valid.
+     * @param username
+     * @param password
+     */
+    public HumanUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String getName() {
+        return username;
+    }
+}
