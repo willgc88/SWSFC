a0596f11c52cb7857c50a75e877c5b84abcd301d
willgc88 <william.clayton@mail.utoronto.ca>
2023-10-23 17:57:54 -0400

    main + entities

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,5 +1,15 @@
 package entity;
 
-class Player {
+class Player implements TeamEntity {
+
+    final String firstname;
+    final String lastname;
+    final String nation;
+    final int age;
+    Team team;
+
+    final int number;
+    final int playerID;
+    PlayerStats playerStats;
 
 }
---
+++ src/entity/TeamEntity.java
@@ -0,0 +1,11 @@
+package entity;
+
+public interface TeamEntity {
+
+    String getFirstname();
+    String getLastname();
+    String getNation();
+    int getAge();
+    Team getTeam();
+
+}
