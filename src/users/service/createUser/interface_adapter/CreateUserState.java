efaec2e1f28bbdc4b87558c627bf42875ca32c25
Shayan Dsouza <dsouza.shayan@gmail.com>
2023-10-30 18:50:21 -0400

    Completed Create User interface adapters as well as ViewModel

--- src/users/service/createUser/interface_adapter/CreateUserState.java
+++ src/users/service/createUser/interface_adapter/CreateUserState.java
@@ -1,4 +1,36 @@
 package users.service.createUser.interface_adapter;
 
 public class CreateUserState {
+    private String username = "";
+    private String usernameError = null;
+
+    public CreateUserState(CreateUserState copy) {
+        username = copy.username;
+        usernameError = copy.usernameError;
+    }
+    public CreateUserState() {
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUsernameError() {
+        return usernameError;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUsernameError(String usernameError) {
+        this.usernameError = usernameError;
+    }
+    @Override
+    public String toString() {
+        return "CreateUserState{" +
+                "username='" + username + '\'' +
+                '}';
+    }
 }
+
+
